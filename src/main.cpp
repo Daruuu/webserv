a726033a0eef6a2e35fe4f2e7dbcdb8594085f4b
Daruuu <thedaruny16@gmail.com>
2025-12-03 22:53:08 +0100

    [DARUUU] fix error with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <exception>
 #include <iostream>
-#include <signal.h>
+#include <csignal>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
 	((void)argc, (void)argv);
-	
+
 	// Disable SIGPIPE globally to prevent crashes
 	// this will be for the CGI part, but already having it here doesn't hurt
 	signal(SIGPIPE, SIG_IGN);
 
-	std::cout << "Esto se pone interesante" << std::endl;
+	std::cout << "Esto se pone interesante. :))" << std::endl;
 
-	try {
+	try
+	{
 		// load configuration into webserver
 		// execute webserver
-	} catch (std::exception& e) {
+	}
+	catch (std::exception& e)
+	{
 		// ooh fuck! something wrong happend
 		// you can do it better next time :)
-	 }
-
+	}
+	return 0;
 }
