b0f03fba16d8bf404d526cd3b6db57ab4bef0bf2
Daruuu <thedaruny16@gmail.com>
2026-02-03 00:08:44 +0100

    [DARU-CONFIG:CONFIG] update ostream more specific

--- src/config/LocationConfig.cpp
+++ src/config/LocationConfig.cpp
@@ -1,6 +1,5 @@
 #include "LocationConfig.hpp"
 #include <iostream>
-#include <algorithm>
 
 LocationConfig::LocationConfig() :
 	autoindex_(false)
@@ -11,7 +10,7 @@ LocationConfig::LocationConfig(const LocationConfig& other) :
 	path_(other.path_),
 	root_(other.root_),
 	index_(other.index_),
-	methods_(other.methods_),
+	allowed_methods_(other.allowed_methods_),
 	autoindex_(other.autoindex_),
 	upload_store_(other.upload_store_),
 	redirect_(other.redirect_)
@@ -25,7 +24,7 @@ LocationConfig& LocationConfig::operator=(const LocationConfig& other)
 		path_ = other.path_;
 		root_ = other.root_;
 		index_ = other.index_;
-		methods_ = other.methods_;
+		allowed_methods_ = other.allowed_methods_;
 		autoindex_ = other.autoindex_;
 		upload_store_ = other.upload_store_;
 		redirect_ = other.redirect_;
@@ -47,31 +46,55 @@ void LocationConfig::addIndex(const std::string& index)
 
 void LocationConfig::addMethod(const std::string& method)
 {
-	methods_.push_back(method);
+	allowed_methods_.push_back(method);
 }
 
 void LocationConfig::setAutoIndex(bool autoindex) { autoindex_ = autoindex; }
-void LocationConfig::setUploadStore(const std::string& store) { upload_store_ = store; }
-void LocationConfig::setRedirect(const std::string& redirect) { redirect_ = redirect; }
+
+void LocationConfig::setUploadStore(const std::string& store)
+{
+	upload_store_ = store;
+}
+
+void LocationConfig::setRedirect(const std::string& redirect)
+{
+	redirect_ = redirect;
+}
 
 const std::string& LocationConfig::getPath() const { return path_; }
 const std::string& LocationConfig::getRoot() const { return root_; }
-const std::vector<std::string>& LocationConfig::getIndexes() const { return index_; }
-const std::vector<std::string>& LocationConfig::getMethods() const { return methods_; }
+
+const std::vector<std::string>& LocationConfig::getIndexes() const
+{
+	return index_;
+}
+
+const std::vector<std::string>& LocationConfig::getMethods() const
+{
+	return allowed_methods_;
+}
+
 bool LocationConfig::getAutoIndex() const { return autoindex_; }
-const std::string& LocationConfig::getUploadStore() const { return upload_store_; }
+
+const std::string& LocationConfig::getUploadStore() const
+{
+	return upload_store_;
+}
+
 const std::string& LocationConfig::getRedirect() const { return redirect_; }
 
 bool LocationConfig::isMethodAllowed(const std::string& method) const
 {
-	if (methods_.empty())
-		return false; // Default safe policy? Or allow all? Usually allow all if empty?
+	if (!allowed_methods_.empty())
+	{
+		// Default safe policy? Or allow all? Usually allow all if empty?
 		// Nginx default is GET only if no limit_except.
 		// For now, let's assume if empty, we might need default, but returning false is safer.
-	for (size_t i = 0; i < methods_.size(); ++i)
-	{
-		if (methods_[i] == method)
-			return true;
+		for (size_t i = 0; i < allowed_methods_.size(); ++i)
+		{
+			if (allowed_methods_[i] == method)
+				return true;
+		}
 	}
 	return false;
 }
@@ -79,25 +102,4 @@ bool LocationConfig::isMethodAllowed(const std::string& method) const
 void LocationConfig::print() const
 {
 	std::cout << *this << std::endl;
-}
-
-std::ostream& operator<<(std::ostream& os, const LocationConfig& config)
-{
-	os << "  Location: " << config.path_ << "\n"
-	   << "    Root: " << config.root_ << "\n"
-	   << "    AutoIndex: " << (config.autoindex_ ? "on" : "off") << "\n"
-	   << "    Upload Store: " << config.upload_store_ << "\n"
-	   << "    Redirect: " << config.redirect_ << "\n";
-	
-	os << "    Indexes: ";
-	for (size_t i = 0; i < config.index_.size(); ++i)
-		os << config.index_[i] << " ";
-	os << "\n";
-
-	os << "    Methods: ";
-	for (size_t i = 0; i < config.methods_.size(); ++i)
-		os << config.methods_[i] << " ";
-	os << "\n";
-
-	return os;
 }
--- src/config/LocationConfig.hpp
+++ src/config/LocationConfig.hpp
@@ -1,19 +1,22 @@
 #ifndef WEBSERV_LOCATIONCONFIG_HPP
 #define WEBSERV_LOCATIONCONFIG_HPP
 
+#include <iostream>
 #include <string>
 #include <vector>
+
+#include "ConfigUtils.hpp"
 
 class LocationConfig
 {
 private:
-	std::string path_;                    // /upload, /, /api, etc.
-	std::string root_;                    // root ./www
-	std::vector<std::string> index_;      // index index.html index.htm ...
-	std::vector<std::string> methods_;    // methods GET POST DELETE
-	bool autoindex_;                      // autoindex on/off
-	std::string upload_store_;            // upload_store ./uploads
-	std::string redirect_;                // return 301 /new-path (optional)
+	std::string path_; // /upload, /, /api, etc.
+	std::string root_; // root ./www
+	std::vector<std::string> index_; // index index.html index.htm ...
+	std::vector<std::string> allowed_methods_; // methods GET POST DELETE
+	bool autoindex_; // autoindex on/off
+	std::string upload_store_; // upload_store ./uploads
+	std::string redirect_; // return 301 /new-path (optional)
 
 public:
 	LocationConfig();
@@ -39,15 +42,49 @@ public:
 	const std::string& getUploadStore() const;
 	const std::string& getRedirect() const;
 
-
 	// Validation
 	bool isMethodAllowed(const std::string& method) const;
 	// bool isValid() const;
-	
+
 	// Debug
 	void print() const;
-
-	friend std::ostream& operator<<(std::ostream& os, const LocationConfig& config);
 };
 
-#endif //WEBSERV_LOCATIONCONFIG_HPP
+inline std::ostream& operator<<(std::ostream& os,
+								const LocationConfig& location)
+{
+	os << config::colors::cyan << "Locations info:\n"
+		<< config::colors::reset << "\tLocation Path: " << location.getPath()
+		<< "\n"
+		<< "\tRoot: " << location.getRoot() << "\n"
+		<< "\tAutoindex: " << (location.getAutoIndex() ? "on" : "off") << "\n";
+
+	if (!location.getUploadStore().empty())
+	{
+		os << "\tUpload Store: " << location.getUploadStore() << "\n";
+	}
+	if (!location.getRedirect().empty())
+	{
+		os << "\tRedirect: " << location.getRedirect() << "\n";
+	}
+
+	const std::vector<std::string>& indexes = location.getIndexes();
+
+	os << "\tIndexes: ";
+	for (size_t i = 0; i < indexes.size(); ++i)
+	{
+		os << indexes[i] << (i == indexes.size() - 1 ? "" : ", ");
+	}
+	// Imprimir Métodos
+	const std::vector<std::string>& methods = location.getMethods();
+	os << "\n\tMethods: ";
+	for (size_t i = 0; i < methods.size(); ++i)
+	{
+		os << methods[i] << (i == methods.size() - 1 ? "" : ", ");
+	}
+	os << "\n";
+
+	return os;
+}
+
+#endif // WEBSERV_LOCATIONCONFIG_HPP
